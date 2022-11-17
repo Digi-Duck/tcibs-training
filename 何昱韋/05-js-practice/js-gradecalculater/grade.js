4b40ac733022eea1a52893f8e283d6f0832055fb
呱呱 <99080074+handsomeCreator@users.noreply.github.com>
2022-11-17 21:02:15 +0800

    gradecalculator

---
+++ 何昱韋/05-js-practice/js-gradecalculater/grade.js
@@ -0,0 +1,15 @@
+var gradevalue;
+
+function Gradecommenter (gradevalue){
+    if (gradevalue >= 90){
+        // very nice SS
+    }else if(gradevalue >= 80){
+        // nice A
+    }else if(gradevalue >= 70){
+        // soso B
+    }else if(gradevalue >= 60){
+        // emmm C
+    }else{
+        // worth D
+    }
+}
