68d4ff715751e5ad58a867f7ff9fb316d7d7cb0e
Jjca329 <Jessica226226@gmail.com>
2022-12-02 21:24:22 +0800

    猜拳進入js

---
+++ 陳宇潔/js/2022.12.02生成亂數.js
@@ -0,0 +1,15 @@
+//  function getRandomInt(max) {
+//              return Math.floor(Math.random() * max);
+//          }
+//         Math.floor(Math.random()*要生成的數字有幾個) + 開始的第一個數字
+//          比如說199~400 總共202個數字
+//         Math.floor(Math.random()*202) + 199
+//          8511~44400 總共35889個數字
+//         Math.floor(Math.random()*35889) + 8511
+
+//          console.log(getRandomInt(3));
+//           expected output: 0, 1 or 2
+//          console.log(Math.random());
+
+
+//          Math.random()
