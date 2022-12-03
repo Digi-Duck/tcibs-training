298cd185000d71970c25a7f167a1adb8c904178b
呱呱 <99080074+handsomeCreator@users.noreply.github.com>
2022-12-03 23:50:38 +0800

    先這樣

---
+++ 何昱韋/05-js-practice/js-剪刀石頭不/game.js
@@ -0,0 +1,11 @@
+var scissor_index,stone_index,no_index;
+var scissor_index = 1;
+function game(scissor_index,stone_index,no_index){
+    if(scissor_index == 1){
+        
+    }else if(stone_index == 1){
+
+    }else if(no_index == 1){
+
+    }
+}
