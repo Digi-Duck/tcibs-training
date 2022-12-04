fe2de227674a5a437004b41a273a7ef16d26a6b6
呱呱 <99080074+handsomeCreator@users.noreply.github.com>
2022-12-04 23:42:07 +0800

    先這樣

--- 何昱韋/05-js-practice/js-剪刀石頭不/game.js
+++ 何昱韋/05-js-practice/js-剪刀石頭不/game.js
@@ -1,6 +1,6 @@
 var selector = Math.floor(Math.random()*2);
-// if selector = 0 --> win
-// if selector = 1 --> lose
+// if selector = 0 --> lose
+// if selector = 1 --> win
 // if selector = 2 --> equal
 if(selector == 0){
     
@@ -9,3 +9,45 @@ if(selector == 0){
 }else if(selector == 2){
 
 }
+function test(){
+    alert("asdf");
+}
+
+function scissorBtn(){
+    var selector = Math.floor(Math.random()*3);
+    if(selector == 0){
+        console.log("lose");
+    }else if(selector == 1){
+        console.log("win");
+    }else if(selector == 2){
+        console.log("equal");
+    }
+}
+function stoneBtn(){
+    var selector = Math.floor(Math.random()*3);
+    if(selector == 0){
+        console.log("lose");
+    }else if(selector == 1){
+        console.log("win");
+    }else if(selector == 2){
+        console.log("equal");
+    }
+}
+function clothBtn(){
+    var selector = Math.floor(Math.random()*3);
+    if(selector == 0){
+        console.log("lose");
+    }else if(selector == 1){
+        console.log("win");
+    }else if(selector == 2){
+        console.log("equal");
+    }
+}
+
+
+let scissor_btn = document.querySelector(".scissor_btn");
+let stone_btn = document.querySelector(".stone_btn");
+let cloth_btn = document.querySelector(".cloth_btn");
+scissor_btn.addEventListener("click",scissorBtn);
+stone_btn.addEventListener("click",stoneBtn);
+cloth_btn.addEventListener("click",clothBtn);
