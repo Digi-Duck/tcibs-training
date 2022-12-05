2b6429adbbb8dc50b74dc393e4c3a947c810eabe
呱呱 <99080074+handsomeCreator@users.noreply.github.com>
2022-12-05 23:24:39 +0800

    11/24

--- 何昱韋/05-js-practice/js-剪刀石頭不/game.js
+++ 何昱韋/05-js-practice/js-剪刀石頭不/game.js
@@ -1,40 +1,54 @@
 let win_index=0,lose_index=0,round=0;
 let roundDisplay = document.querySelector(".round");
+let winDisplay = document.querySelector(".win_score");
+let loseDisplay = document.querySelector(".lose_score");
 function scissorBtn(){
     var selector = Math.floor(Math.random()*3);
     if(selector == 0){
         console.log("lose");
+        lose_index++;
     }else if(selector == 1){
         console.log("win");
+        win_index++
     }else if(selector == 2){
         console.log("equal");
     }
     round++;
     roundDisplay.innerHTML ="回合" + round;
+    winDisplay.innerHTML = "贏:" + win_index +"場";
+    loseDisplay.innerHTML = "輸:" + lose_index +"場";
 }
 function stoneBtn(){
     var selector = Math.floor(Math.random()*3);
     if(selector == 0){
         console.log("lose");
+        lose_index++;
     }else if(selector == 1){
         console.log("win");
+        win_index++;
     }else if(selector == 2){
         console.log("equal");
     }
     round++;
     roundDisplay.innerHTML ="回合" + round;
+    winDisplay.innerHTML = "贏:" + win_index +"場";
+    loseDisplay.innerHTML = "輸:" + lose_index +"場";
 }
 function clothBtn(){
     var selector = Math.floor(Math.random()*3);
     if(selector == 0){
         console.log("lose");
+        lose_index++;
     }else if(selector == 1){
         console.log("win");
+        win_index++;
     }else if(selector == 2){
         console.log("equal");
     }
     round++;
     roundDisplay.innerHTML ="回合" + round;
+    winDisplay.innerHTML = "贏:" + win_index +"場";
+    loseDisplay.innerHTML = "輸:" + lose_index +"場";
 }
 
 
