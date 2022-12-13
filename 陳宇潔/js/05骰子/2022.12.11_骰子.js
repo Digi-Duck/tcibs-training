638bd7529e2407d6afac565a4f10d238ed42e6a5
Jjca329 <114589189+Jjca329@users.noreply.github.com>
2022-12-13 16:02:37 +0800

    哈哈

--- 陳宇潔/js/05骰子/2022.12.11_骰子.js
+++ 陳宇潔/js/05骰子/2022.12.11_骰子.js
@@ -2,29 +2,34 @@ const start = document.querySelector(".start")
 const re = document.querySelector(".reload")
 const group = document.querySelector(".row")
 
+let i = 0;
+
 start.onclick = function(){
-    let dice = Math.floor(Math.random()*6 + 1)
-    console.log(dice)
-    switch(dice){
-        case 1:
-            group.innerHTML=`<img src="./dice_image/1.png" class="dice pic">`
-            break;
-        case 2:
-            group.innerHTML=`<img src="./dice_image/2.png" class="dice pic">`
-            break;
-        case 3:
-            group.innerHTML=`<img src="./dice_image/3.png" class="dice pic">`
-            break;
-        case 4:
-            group.innerHTML=`<img src="./dice_image/4.png" class="dice pic">`
-            break;
-        case 5:
-            group.innerHTML=`<img src="./dice_image/5.png" class="dice pic">`
-            break;
-        case 6:
-            group.innerHTML=`<img src="./dice_image/6.png" class="dice pic">`
-            break;
-    }
+    for(let i = 0; i < 3; i++){
+        let dice = Math.floor(Math.random()*6 + 1)
+        
+        switch(dice){
+            case 1:
+                group.innerHTML += `<img src="./dice_image/1.png" class="dice pic">`
+                break;
+            case 2:
+                group.innerHTML += `<img src="./dice_image/2.png" class="dice pic">`
+                break;
+            case 3:
+                group.innerHTML += `<img src="./dice_image/3.png" class="dice pic">`
+                break;
+            case 4:
+                group.innerHTML += `<img src="./dice_image/4.png" class="dice pic">`
+                break;
+            case 5:
+                group.innerHTML += `<img src="./dice_image/5.png" class="dice pic">`
+                break;
+            case 6:
+                group.innerHTML += `<img src="./dice_image/6.png" class="dice pic">`
+                break;
+        }
+        console.log(dice)
+    }   
 }
 re.onclick = function(){
     setTimeout(() => window.location.reload(),500)
