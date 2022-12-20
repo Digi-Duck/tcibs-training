bbafe8a0b2d2df98f8219d79e9bddf04f069cb5b
Jjca329 <114589189+Jjca329@users.noreply.github.com>
2022-12-20 21:17:49 +0800

    色弱

--- 陳宇潔/js/07色弱01/2022.12.18_色弱.js
+++ 陳宇潔/js/07色弱01/2022.12.18_色弱.js
@@ -1,15 +1,45 @@
 const start = document.querySelector(".btn")
 const box = document.querySelectorAll(".box")
 
-let r = Math.floor(Math.random() * 256);
-let g = Math.floor(Math.random() * 256);
-let b = Math.floor(Math.random() * 256);
+function getrandom(){
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    x = Math.floor(Math.random() * 4)
+    return r,g,b,x;
+}
+// function trans(){
+//     getrandom()
+//     box.forEach(function(index,element){
+//         box[element].style.backgroundColor = "rgb("+ r + "," + g + "," + b + ")" ;      
+//         box[x].style.opacity = "0.1";
+//         console.log(x);
+//     })
+//     console.log(box[x]);
+// }
 
-start.addEventListener("click", function(){
-    let x = Math.floor(Math.random() * 4);
-    box.forEach(function(index,element){
-        box[element].style.backgroundColor = "rgb("+ r + "," + g + "," + b + ")" ;      
-        box[x].style.opacity = "0.1";
-    })
-})
+// start.addEventListener("click", function(){
+//     trans();
+//     console.log(box[x]);
+//     box[x].addEventListener("click", function(){
+//         box[x].style.opacity = "1";
+//         trans();
+//     })
+// })
 
+
+
+start.addEventListener("click", 
+    function trans(){
+        getrandom()
+        box.forEach(function(index,element){
+            box[element].style.backgroundColor = "rgb("+ r + "," + g + "," + b + ")" ;      
+            box[x].style.opacity = "0.1";
+            
+            box[x].addEventListener("click", function(){
+                box[x].style.opacity = "1";
+                trans();
+            })
+        })
+    }
+)
