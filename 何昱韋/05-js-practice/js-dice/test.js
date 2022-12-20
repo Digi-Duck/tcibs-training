4083f15c27b47001b79a0a907966a69e07ac6cc5
呱呱 <99080074+handsomeCreator@users.noreply.github.com>
2022-12-20 20:50:11 +0800

    nice
    display:ok

--- 何昱韋/05-js-practice/js-dice/test.js
+++ 何昱韋/05-js-practice/js-dice/test.js
@@ -8,19 +8,20 @@ const diceTemplate = {
 }
 function gameResult() {
     let ADGameRegion = document.querySelector(".AD_dice_region_part");
+    let MYGameRegion = document.querySelector(".MY_dice_region_part");
     let data = [];
     let dataLen = 10;
+    let ADTotalIndex = 0;
     for (let rowIndex = 0; rowIndex < dataLen; rowIndex += 1) {
         let row = [];
         for (let _ = 0; _ < 3; _ += 1){
             let num = Math.floor(Math.random() * 6) + 1;
             row.push(num);
-            ADGameRegion.appendChild(diceTemplate[num]);
-            console.log(diceTemplate[num]);
+            ADGameRegion.innerHTML += String(diceTemplate[num].outerHTML);
+            ADTotalIndex = ADTotalIndex + num;
         }
         data[rowIndex] = row;
     }
-    console.log(data);
 }
 
 let throw_btn = document.querySelector(".throw_btn");
