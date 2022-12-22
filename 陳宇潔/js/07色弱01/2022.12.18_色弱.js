2501d20bc85011af09f81fa4df18f41054c8eff8
Jjca329 <114589189+Jjca329@users.noreply.github.com>
2022-12-22 21:30:53 +0800

    馬的色弱  我卡在一樣的地方四個小時 原來是監聽事件在搞

--- 陳宇潔/js/07色弱01/2022.12.18_色弱.js
+++ 陳宇潔/js/07色弱01/2022.12.18_色弱.js
@@ -5,41 +5,37 @@ function getrandom(){
     r = Math.floor(Math.random() * 256);
     g = Math.floor(Math.random() * 256);
     b = Math.floor(Math.random() * 256);
-    x = Math.floor(Math.random() * 4)
+    x = Math.floor(Math.random() * 4);
     return r,g,b,x;
 }
-// function trans(){
-//     getrandom()
-//     box.forEach(function(index,element){
-//         box[element].style.backgroundColor = "rgb("+ r + "," + g + "," + b + ")" ;      
-//         box[x].style.opacity = "0.1";
-//         console.log(x);
-//     })
-//     console.log(box[x]);
-// }
 
-// start.addEventListener("click", function(){
-//     trans();
-//     console.log(box[x]);
-//     box[x].addEventListener("click", function(){
-//         box[x].style.opacity = "1";
-//         trans();
-//     })
-// })
+function trans(){ //改變
+    // 產生變數
+    getrandom();
+    console.log(x);
+    // 所有的方塊跑回圈
+    box.forEach(function(element){
+        element.style.backgroundColor = "rgb("+ r + "," + g + "," + b + ")";      
+    })
+    // 改變透明度
+    box[x].style.opacity = "0.1";
+
+    // 綁定監聽事件
+    box[x].addEventListener("click", function(){
+        box[x].style.opacity = "1";
+        trans();
+    })
+}
+
+function a(){ //迴圈
+
+}
 
 
-
-start.addEventListener("click", 
-    function trans(){
-        getrandom()
-        box.forEach(function(index,element){
-            box[element].style.backgroundColor = "rgb("+ r + "," + g + "," + b + ")" ;      
-            box[x].style.opacity = "0.1";
-            
-            box[x].addEventListener("click", function(){
-                box[x].style.opacity = "1";
-                trans();
-            })
-        })
-    }
-)
+start.addEventListener("click", function (){
+    start.classList.add("disappear")
+    trans();
+    // console.log(111);
+    // console.log(x);
+    
+})
