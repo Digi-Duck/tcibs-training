5da4818f35080b1fdb1424ae263f3faf46b90af9
Jjca329 <114589189+Jjca329@users.noreply.github.com>
2022-12-23 15:45:20 +0800

    朱進益快debug

--- 陳宇潔/js/07色弱01/2022.12.18_色弱.js
+++ 陳宇潔/js/07色弱01/2022.12.18_色弱.js
@@ -2,44 +2,32 @@ const start = document.querySelector(".btn")
 const box = document.querySelectorAll(".box")
 
 function getrandom(){
-    r = Math.floor(Math.random() * 256);
-    g = Math.floor(Math.random() * 256);
-    b = Math.floor(Math.random() * 256);
-    x = Math.floor(Math.random() * 4)
+     r = Math.floor(Math.random() * 256);
+     g = Math.floor(Math.random() * 256);
+     b = Math.floor(Math.random() * 256);
+     x = Math.floor(Math.random() * 4)
     return r,g,b,x;
 }
-// function trans(){
-//     getrandom()
-//     box.forEach(function(index,element){
-//         box[element].style.backgroundColor = "rgb("+ r + "," + g + "," + b + ")" ;      
-//         box[x].style.opacity = "0.1";
-//         console.log(x);
-//     })
-//     console.log(box[x]);
-// }
 
-// start.addEventListener("click", function(){
-//     trans();
-//     console.log(box[x]);
-//     box[x].addEventListener("click", function(){
-//         box[x].style.opacity = "1";
-//         trans();
-//     })
-// })
+start.addEventListener('click', function() {
+    start.removeEventListener('click', () => {})
+    again();
+})
 
 
-
-start.addEventListener("click", 
-    function trans(){
-        getrandom()
-        box.forEach(function(index,element){
-            box[element].style.backgroundColor = "rgb("+ r + "," + g + "," + b + ")" ;      
-            box[x].style.opacity = "0.1";
-            
-            box[x].addEventListener("click", function(){
-                box[x].style.opacity = "1";
-                trans();
-            })
-        })
-    }
-)
+function again(){
+    getrandom();
+    box.forEach(element => {
+        element.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")"; 
+        element.classList.remove("opacity") ;
+        element.removeEventListener("click", () => {})
+    });
+    let opacity = box[x]
+    opacity.classList.add("opacity");
+    
+    opacity.addEventListener('click', function() {
+        opacity.removeEventListener('click', () => {})
+        again();
+    })
+    
+}
