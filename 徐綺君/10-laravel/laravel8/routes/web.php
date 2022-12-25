dcbf2ffa7a7f0d04b745a8b8032dbb1731908057
jenny10603 <114579336+jenny10603@users.noreply.github.com>
2022-12-25 15:16:22 +0800

    1

--- 徐綺君/10-laravel/laravel8/routes/web.php
+++ 徐綺君/10-laravel/laravel8/routes/web.php
@@ -16,3 +16,60 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',function(){
+
+    // dd('123',12);
+    //dump die
+
+    // $box = '123';
+
+    // dd($box1 ?? '123');
+
+    // if($box == 0){
+    //     dd('1');
+    // }elseif($box == 1){
+    //     dd('1');
+    // }else{
+    //     dd('0');
+    // }
+
+    // for($i = 0; $i < 10; $i++){
+    //     $number += $i;
+    // }
+
+    // dd($number,$i);
+
+    // $box = ['a',['b'],['c',['d'],'e'],'f','g'];
+    // //d
+    // dd($box[2][1][0]);
+
+    // $box = ['abd'=>'123'];
+    // dd($box['abc']);
+    //"123"
+
+    // $box = [1,2,3,4,5,6,7];
+    // foreach($box as $item){
+    //     echo $item;
+    //     // dump($item);
+    // };
+
+    // $box = ['A','B','C','D','E','F','G'];
+    // foreach($box as $index =>$value){
+    //     echo($index);
+    //     echo($value);
+    //     dump($item);
+    // };
+
+    // return '123';
+});
+
+// Route::get('/hello/{number}',function($number){
+//     dd($number);
+//     return '123';
+// });
+
+Route::get('/hello/{number1}',function($a = null){
+    dd($a);
+    // return '123';
+});
