b786eb2616225006e633c8a821975ccd40bb4972
DonDonDonDonDonDonDonDon <103096544+DonDonDonDonDonDonDonDon@users.noreply.github.com>
2022-12-26 19:57:54 +0800

    1

---
+++ 朱勁屹/15.Laravel/laravel8/app/Http/Controllers/FrontController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    //
+    // public function hello()
+    // {
+    //     return view('hello');
+    // }
+
+    public function hello()
+    {
+        
+        return view('hello');
+    }
+}
---
+++ 朱勁屹/15.Laravel/laravel8/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\FrontController;//導入
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/hello',function(){
+//     $name = 'bitchs';
+//     $a = 'smallBitch';
+//     $b = 'bigBitch';
+//     compact('name','a','b');
+//     // return view('hello',['name' => $name]);
+//     return view('hello',compact('name','a','b'));
+// });
+
+Route::get('/hello',[FrontController::class,'hello']);
+// Route::get('/hello','FrontController@hello');
+
