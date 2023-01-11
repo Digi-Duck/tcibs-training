9c28db8794714cfd51cae374e18dc0614a0c2e45
DonDonDonDonDonDonDonDon <103096544+DonDonDonDonDonDonDonDon@users.noreply.github.com>
2023-01-11 19:47:22 +0800

    1

--- 朱勁屹/15.Laravel/laravel8/resources/views/admin/news/index.blade.php
+++ 朱勁屹/15.Laravel/laravel8/resources/views/admin/news/index.blade.php
@@ -14,45 +14,54 @@
             padding: 5px;
             text-decoration: none;
         }
-        tr{
+        td{
             text-align: center;
+        }
+        .main{
+            width: 80%;
+            margin: auto;
+        }
+        .text-center{
+            margin: auto;
         }
     </style>
 </head>
 <body>
-
-    <table id="myTable" class="display">
-        <thead>
-            <tr>
-                <th>標題</th>
-                <th>日期</th>
-                <th>內容</th>
-                <th>圖片</th>
-                <th>操作</th>
+    <div class="main">
+        <table id="myTable" class="display">
+            <thead>
+                <tr class="tr">
+                    <th>標題</th>
+                    <th>日期</th>
+                    <th>內容</th>
+                    <th>圖片</th>
+                    <th width="120">操作</th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($news as $item)
+                <tr>
+                <td>{{$item->title}}</td>
+                <td>{{$item->date}}</td>
+                <td>{{$item->content}}</td>
                 
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($news as $item)
-            <tr>
-            <td>{{$item->title}}</td>
-            <td>{{$item->date}}</td>
-            <td>{{$item->content}}</td>
-            
-            <td><img src="{{$item->image}}" alt="" width="200" height="200"></td>
-            <td>
-                <a href="" class="btn btn-primary">編輯</a>
-                <a href="" class="btn btn-danger">刪除</a>
-            </td>
-            </tr>
-            @endforeach
-            
-            {{-- <tr>
-                <td>Row 2 Data 1</td>
-                <td>Row 2 Data 2</td>
-            </tr> --}}
-        </tbody>
-    </table>
+                <td><img src="{{$item->image}}" alt="" width="200" height="200"></td>
+                <td class="text-center">
+                    <a href="" class="btn btn-primary">編輯</a>
+                    <a href="" class="btn btn-danger">刪除</a>
+                </td>
+                </tr>
+                @endforeach
+                
+                {{-- <tr>
+                    <td>Row 2 Data 1</td>
+                    <td>Row 2 Data 2</td>
+                </tr> --}}
+            </tbody>
+        </table>
+
+    </div>
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <script>
