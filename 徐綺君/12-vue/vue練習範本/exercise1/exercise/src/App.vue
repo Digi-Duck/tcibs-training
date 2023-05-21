df9b9efbdb129baca4103a39222a67a20508a4c0
jenny10603 <114579336+jenny10603@users.noreply.github.com>
2023-05-21 13:28:50 +0800

    1

--- 徐綺君/12-vue/vue練習範本/exercise1/exercise/src/App.vue
+++ 徐綺君/12-vue/vue練習範本/exercise1/exercise/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App 0.0"/>
+  <h1>dsfjlk;dsdlfjiejfsz;ads</h1>
+  <h2>dfjsjdfjepojfjdslkfklmcsdjfldmkfjlsdf</h2>
+  <h3>788945561212345ooooskdsifdjhfjrfdsddvdfdjdkndkdjsdjkjdfjfiwfdsijdhdfkjjkdfssdfljjjrujkfg</h3>
 </template>
 
 <script>
