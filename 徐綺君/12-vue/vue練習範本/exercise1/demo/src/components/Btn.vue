72eed0a9a7a172e51da5fabd0cb04dce1de9cf68
jenny10603 <114579336+jenny10603@users.noreply.github.com>
2023-05-23 19:45:10 +0800

    1

--- 徐綺君/12-vue/vue練習範本/exercise1/demo/src/components/Btn.vue
+++ 徐綺君/12-vue/vue練習範本/exercise1/demo/src/components/Btn.vue
@@ -1,3 +1,27 @@
 <template>
-    <button>1234</button>
+    <button class="btn">{{ btnText }}</button>
 </template>
+
+<script>
+export default {
+  name: 'btn',
+  props: {
+    btnText: String
+  }
+}
+</script>
+
+<style scoped>
+    button{
+        background-color: rgb(255, 255, 255);
+        border: none;
+        box-shadow: 1px 1px 1px 2px rgb(165, 165, 165);
+        border-radius: 5px;
+        padding: 5px;
+        margin: 5px;
+        transition: 50ms;
+    }
+    button:hover{
+        box-shadow: -1px -1px 1px 2px rgb(165, 165, 165);
+    }
+</style>
--- 徐綺君/12-vue/vue練習範本/exercise1/demo/src/views/CounterView.vue
+++ 徐綺君/12-vue/vue練習範本/exercise1/demo/src/views/CounterView.vue
@@ -2,10 +2,9 @@
     <div class="counter">
         <h1>計數器</h1>
         set Number <input type="number" v-model.number="num">
-        <button @click="add">+1</button>
-        <button @click="reduce">-1</button>
-        <button @click="clear">歸零</button>
-        <btn />
+        <btn @click="add" btnText="+1" />
+        <btn @click="reduce" btnText="-1" />
+        <btn @click="clear" btnText="歸零" />
     </div>
 </template>
 
