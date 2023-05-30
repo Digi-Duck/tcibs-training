4a0f4df3d3a2b7ca3dd3cd639c8eb5c4566aa335
DonDonDonDonDonDonDonDon <103096544+DonDonDonDonDonDonDonDon@users.noreply.github.com>
2023-05-30 20:17:13 +0800

    1

--- 朱勁屹/18.vue/fanally/demo/src/views/2.vue
+++ 朱勁屹/18.vue/fanally/demo/src/views/2.vue
@@ -1,10 +1,47 @@
 <template>
+{{ array['a']['a'] }}
+<form action="">
+    <div class="box w-[500px] h-[500px] border-8 rounded m-[auto]">
+        <div class="text w-[20%] h-[25%] border-2 float-left"></div> 
+        <input type="text" class="">
 
-    
+        <div class="text w-[20%] h-[25%] border-2 float-left"></div> 
+        <input type="text">
+
+        <div class="text w-[20%] h-[25%] border-2 float-left"></div> 
+        <input type="text">
+
+        <div class="text w-[20%] h-[25%] border-2 float-left"></div> 
+        <input type="text">
+        
+        <button class="">送出</button>
+    </div>
+
+</form>
 </template>
 <script>
+    import {ref} from 'vue';
+    
+    export default{
+        setup(){
+            const array = ref(
+                {
+                    'a':{
+                        'birth' : '',
+                        'bodyId' : '4521532485',
+                        'address' : '鳳梨市鳳梨區鳳梨巷鳳梨樓',
+                        'time':[],
+                    }
+                }
+            );
 
+            
+            return{
+                array,
+            }
+        }
+    }
 </script>
 <style>
-
+    
 </style>
