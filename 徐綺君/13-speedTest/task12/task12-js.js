41dc65a1ff975ddf2bcc4fba38c8cd211b3a7086
jenny10603 <114579336+jenny10603@users.noreply.github.com>
2023-06-04 13:02:49 +0800

    1

---
+++ 徐綺君/13-speedTest/task12/task12-js.js
@@ -0,0 +1,30 @@
+const imgGroup = document.querySelectorAll('.img');
+const back = document.querySelector('.back');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const lightImg = document.querySelector('.lightImg');
+const lightBox = document.querySelector('.lightBox');
+
+imgGroup.forEach((img,i) => {
+    img.addEventListener('click',()=>{
+        lightBox.classList.remove('display')
+        lightImg.src = img.src
+        prev.addEventListener('click',()=>{
+            i = i - 1
+            if (i < 0) {
+                i = imgGroup.length - 1;
+            }
+            lightImg.src = imgGroup[i].src
+        })
+        next.addEventListener('click',()=>{
+            i = i + 1
+            if (i > imgGroup.length - 1) {
+                i = 0;
+            }
+            lightImg.src = imgGroup[i].src
+        })
+    })
+});
+back.addEventListener('click',()=>{
+    lightBox.classList.add('display')
+})
