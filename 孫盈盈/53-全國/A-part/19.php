7a18dc6bf597cf28532ae6c3971c75d9497723fe
EmmaEmmEmm <103096574+EmmaEmmEmm@users.noreply.github.com>
2023-06-08 16:31:22 +0800

    e

---
+++ 孫盈盈/53-全國/A-part/19.php
@@ -0,0 +1,24 @@
+<?php
+$base = $_POST['file'];
+$name = 'img.png';
+$path = '/img/img.png';
+
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="'. $name .'"');
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: '.filesize($path));
+
+ob_clean();
+flush();
+
+readfile($path);
+
+
+header('Location: 19.php');
+exit;
+
+?>
