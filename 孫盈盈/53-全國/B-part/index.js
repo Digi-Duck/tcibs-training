e7e0f8a2df85b9383754db15515cc8bd482cd9a0
EmmaEmmEmm <103096574+EmmaEmmEmm@users.noreply.github.com>
2023-06-19 23:16:30 +0800

    b

--- 孫盈盈/53-全國/B-part/index.js
+++ 孫盈盈/53-全國/B-part/index.js
@@ -1,6 +1,9 @@
 function openOb(){
     let card = document.querySelector('.window');
     card.style.display = 'flex';
+
+    let first = document.querySelector('#first');
+    first.style.position = 'unset';
 }
 function addOb(){
     let card = document.querySelector('.window');
@@ -10,7 +13,57 @@ function addOb(){
     let h = document.getElementById('h');
     let first = document.querySelector('#first');
 
-    console.log(first);
+    first.style.position = 'relative';
     first.style.width = w.value + 'px';
     first.style.height = h.value + 'px';
+
+    let layers = document.querySelector('.layers');
+    layers.innerHTML = `<div class="layer">
+                            <input type="text" value="layer1">
+                            <input type="checkbox" name="layer" checked>
+                        </div>`;
 }
+
+let canvas = document.querySelectorAll('canvas');
+let isDrawing = false;
+let [lastX, lastY] = [0, 0];
+
+canvas.forEach((element, index) => {
+    let w = document.getElementById('w');
+    let h = document.getElementById('h');
+    element.width = w.value;
+    element.height = h.value;
+    console.log(element);
+    let ctx = element.getContext('2d');
+
+    element.addEventListener('mousedown', (e) => {
+        isDrawing = true;
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+        element.addEventListener('mousemove', draw);
+    });
+    element.addEventListener('mouseup', () => {
+        isDrawing = false;
+        element.removeEventListener('mousemove', draw);
+    });
+    element.addEventListener('mouseleave', () => {
+        isDrawing = false;
+        element.removeEventListener('mousemove', draw);
+    });
+
+    function draw(e) {
+        let color = document.getElementById('color');
+        let paintWidth = document.getElementById('width');
+
+        ctx.lineWidth = paintWidth.value;
+        ctx.strokeStyle = color.value;
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+    }
+});
