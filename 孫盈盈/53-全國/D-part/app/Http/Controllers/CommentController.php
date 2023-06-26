0870d021d055756b596c8479ec2a5e64745e8d7b
EmmaEmmEmm <103096574+EmmaEmmEmm@users.noreply.github.com>
2023-06-26 09:27:23 +0800

    123

--- 孫盈盈/53-全國/D-part/app/Http/Controllers/CommentController.php
+++ 孫盈盈/53-全國/D-part/app/Http/Controllers/CommentController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Vaildator;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ErrorController;
 
 class CommentController extends Controller
 {
-    //
+    public function addComment(Request $request, $image_id){
+        if(DB::table('image')->where('id', $image_id)->doesntExist()){
+            return ErrorController::picture();
+        }else if(!$request->filled('comment')){
+            return ErrorController::body_null();
+        }else{
+            DB::table('comment')->insert([
+                'image_id'=>$image_id,
+                'comment'=>$request->comment
+            ]);
+            return 'scuess';
+        }
+    }
+    public function selectComment(Request $request, $image_id){
+        if(DB::table('image')->where('id', $image_id)->doesntExist()){
+            return ErrorController::picture();
+        }else{
+            $comment = DB::table('comment')->where('image_id', $image_id)->get();
+            return $comment;
+        }
+    }
+    public function deleteComment(Request $request, $comment_id){
+        if(DB::table('comment')->where('id', $comment_id)->doesntExist()){
+            return ErrorController::comment();
+        }else{
+            DB::table('comment')->where('id', $comment_id)->delete();
+            return 'delete sucess';
+        }
+    }
 }
--- 孫盈盈/53-全國/D-part/routes/api.php
+++ 孫盈盈/53-全國/D-part/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('auth/register', [UserController::class, 'register']);
 Route::post('auth/login', [UserController::class, 'login']);
 Route::post('auth/logout', [UserController::class, 'logout']);
+
+Route::get('images/search', []);
+Route::get('images/popular', []);
+Route::get('users/{user_id}/images', []);
+Route::post('images/upload', []);
+Route::put('images/{image_id}', []);
+Route::post('images/{image_id}', []);
+Route::get('images/{image_id}', []);
+Route::delete('images/{image_id}', []);
+
+Route::get('images/{image_id}/comments', [CommentController::class, 'selectComment']);
+Route::post('images/{image_id}/comments', [CommentController::class, 'addComment']);
+Route::delete('images/comments/{comment_id}', [CommentController::class, 'deleteComment']);
